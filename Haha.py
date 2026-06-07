3d980bd8cf63b7b41391141b0ad3d9d24146ac1b
Nevaeh <nevaeh.c66@icloud.com>
2026-06-06 19:08:59 -0700

    Added debug logging

--- Haha.py
+++ Haha.py
@@ -1,18 +1,23 @@
 import os
 import discord
 from dotenv import load_dotenv
+
 load_dotenv()
-DISCORD_TOKEN_HAHA= os.getenv("DISCORD_TOKEN_HAHA")
+
+# DEBUG: Check if the token is actually there
+token = os.getenv("DISCORD_TOKEN_HAHA")
+if not token:
+    print("CRITICAL ERROR: DISCORD_TOKEN_HAHA is not set!")
+else:
+    print(f"Token found! Length: {len(token)}") 
 
 class MyClient(discord.Client):
-    def __init__(self, *args, **kwargs):
-        # Create an 'intents' object that lets the bot see messages
-        intents = discord.Intents.default()
-        intents.message_content = True 
-        
-        # Pass that 'intents' object to the superclass
-        super().__init__(*args, **kwargs, intents=intents)
+    # ... (your existing class code) ...
+    
+    # Let's add this to see if it even connects
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
 
-# Pass the intents into your client
-client = MyClient()
-client.run(DISCORD_TOKEN_HAHA)
+client = MyClient(intents=discord.Intents.default()) # Make sure you handle intents properly
+client.run(token)
