8475ab9181a72b65e693812a12346f1eeb031b3b
Nevaeh <nevaeh.c66@icloud.com>
2026-06-06 18:12:42 -0700

    Adding required intents to client

--- Haha.py
+++ Haha.py
@@ -3,11 +3,15 @@ import discord
 from dotenv import load_dotenv
 load_dotenv()
 DISCORD_TOKEN_HAHA= os.getenv("DISCORD_TOKEN_HAHA")
+
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # This dictionary stores a separate history for every person
-        self.memories = {}
+        # Create an 'intents' object that lets the bot see messages
+        intents = discord.Intents.default()
+        intents.message_content = True 
+        
+        # Pass that 'intents' object to the superclass
+        super().__init__(*args, **kwargs, intents=intents)
 
 # Pass the intents into your client
 client = MyClient()
